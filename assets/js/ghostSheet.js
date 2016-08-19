463d6e1896afd239d9f36c853cebc82ee313b4e7
teesloane <tylersloane@gmail.com>
2016-08-19 19:52:29 -0400

    added interactive lines to header

--- assets/js/ghostSheet.js
+++ assets/js/ghostSheet.js
@@ -1,3 +1,53 @@
-/* ==========================================================================
-   Add you custom JS here
-   ========================================================================== */
+// Interactivity + Noodling
+  // A. Header Line Interaction
+
+/*=============== A. Interactive Header Lines =================== */
+(function() {
+  console.log('hi');
+  // Target container, append lines, create array of lines.
+  var stackOfLines = document.getElementById('pyramid');
+  createLines(stackOfLines, 7);
+  var lines = Array.prototype.slice.call(stackOfLines.children);
+
+  // set the line width (%) and how much to increment each line by.
+  var lineWidth = 50;
+  var widthIncrement = lineWidth / lines.length;
+
+  // loop through the lines and increment each suceeding line.
+  for(var i = 0; i < lines.length; i++) {
+    lines[i].style.width = lineWidth + "%";
+    lineWidth -= widthIncrement;
+  }
+
+  /* for each line, set the width it should return to,
+  length of the line before it, and format it to be animated */
+  lines.forEach(function(line, idx) {
+    var currentWidth = removePercentage(line.style.width);
+    var previousWidth = lines[lines.length - (idx + 1)].style.width;
+    var newWidthToAnimate = removePercentage(previousWidth);
+
+    line.addEventListener('mouseover', function() {
+      line.style.width = newWidthToAnimate + "%";
+    });
+
+    line.addEventListener('mouseleave', function() {
+      setTimeout(function() {
+        line.style.width = currentWidth + "%";
+      }, 4000);
+    });
+  });
+
+  // helper to work with string'd style values.
+  function removePercentage(string) {
+    string = string.split('%').slice(0, 1)[0];
+    return string;
+  }
+
+  function createLines(parent, numLines) {
+    for (var i = 0; i < numLines; i++) {
+      var line = document.createElement('div');
+      line.classList.add('line');
+      parent.appendChild(line);
+    }
+  }
+})();
